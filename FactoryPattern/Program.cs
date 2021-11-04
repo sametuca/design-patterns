e0a2ca7f89435cd3dadda974c9380e4dc457584d
Samet UCA <sametuca@hotmail.com>
2021-11-04 20:19:32 +0300

    Add project files.

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GameFactory gf = new GameFactory();
+            Game game = gf.FactoryMethod(GameName.GodOfWar);
+            game.Platform();
+        }
+    }
+
+    public interface Game
+    {
+        public abstract void Platform();
+    }
+
+    public class GodOfWar : Game
+    {
+        public void Platform()
+        {
+            Console.WriteLine("Bu oyun ATARİ platformunda çalışmaktadır.");
+        }
+    }
+
+    public class GrandTheftAutoFive : Game
+    {
+        public void Platform()
+        {
+            Console.WriteLine("Bu oyun PC platformunda çalışmaktadır.");
+        }
+    }
+
+    public class RedDeadRedemptionTwo : Game
+    {
+        public void Platform()
+        {
+            Console.WriteLine("Bu oyun PS platformunda çalışmaktadır.");
+        }
+    }
+
+    public enum GameName
+    {
+        GodOfWar,
+        RedDeadRedemptionTwo,
+        GrandTheftAutoFive
+    }
+    public class GameFactory
+    {
+        public Game FactoryMethod(GameName OyunTipi)
+        {
+            Game oyun = null;
+            switch (OyunTipi)
+            {
+                case GameName.GodOfWar:
+                    oyun = new GodOfWar();
+                    break;
+                case GameName.RedDeadRedemptionTwo:
+                    oyun = new RedDeadRedemptionTwo();
+                    break;
+                case GameName.GrandTheftAutoFive:
+                    oyun = new GrandTheftAutoFive();
+                    break;
+            }
+            return oyun;
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PayWrapper pw = new PayWrapper(new CreditCard());
+            pw.MakePayBro();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class PayStrategy
+    {
+        public abstract void MakePay();
+    }
+
+    public class CreditCard : PayStrategy
+    {
+        public override void MakePay()
+        {
+            Console.WriteLine("Kredi kartıyla ödendi");
+        }
+    }
+
+
+    public class HomePay : PayStrategy
+    {
+        public override void MakePay()
+        {
+            Console.WriteLine("Üretim ");
+        }
+    }
+
+    public class PayWrapper
+    {
+        private readonly PayStrategy _strategy;
+
+        public PayWrapper(PayStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void MakePayBro()
+        {
+            _strategy.MakePay();
+        }
+    }
+
+
+
+}
